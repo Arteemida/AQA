9a2229a1f4c365c494b353905fb26aa8d5cec32b
Diana Ofitserova <dofitserova@readdle.com>
2022-11-11 11:36:11 -0500

    - added a method that accepts a string (lesson 9);

--- src/main/java/Homeworks/Lesson_9/Lesson9.java
+++ src/main/java/Homeworks/Lesson_9/Lesson9.java
@@ -7,7 +7,11 @@ import java.util.Set;
 public class Lesson9 {
     public static void main(String[] args) {
         String listValues = "tiger, cat, panther, cat, puma, tiger.";
-        String[] words = listValues.replaceAll("[ .]", "").split(",");
+        Function(listValues);
+    }
+
+    public static void Function(String ListOfValues) {
+        String[] words = ListOfValues.replaceAll("[ .]", "").split(",");
         Set<String> uniqList = new HashSet<>(Arrays.asList(words));
         for (String values : uniqList) {
             System.out.println(values.substring(0, 1).toUpperCase() + values.substring(1));
